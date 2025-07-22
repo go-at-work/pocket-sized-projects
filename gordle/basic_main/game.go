ce550198f2354023ce244be693ae2e8982b6d3e1
arisromil <arisromil@yahoo.com>
2025-07-22 10:13:24 -0600

    Update game.go

--- gordle/basic_main/game.go
+++ gordle/basic_main/game.go
@@ -1,19 +1,58 @@
 package gordle
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
-type Game struct{}
+const solutionLength = 5
 
-func New() *Game {
-	g := &Game{}
+type Game struct {
+	reader      *bufio.Reader
+	solution    []rune
+	maxAttempts int
+}
 
+func New(playerInput io.Reader, solution string, maxAttempts int) *Game {
+	g := &Game{
+		reader:      bufio.NewReader(playerInput),
+		solution:    splitToUppercaseCharacters(solution),
+		maxAttempts: maxAttempts,
+	}
 	return g
+}
+
+func splitToUppercaseCharacters(input string) []rune {
+	return []rune(strings.ToUpper(input))
+}
+
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %s\n", err.Error())
+			continue
+		}
+
+		guess := []rune(string(playerInput))
+
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution! Expected %d characters, got %d.\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
 
 func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 
-	fmt.Printf("Enter a guess:\n")
+	guess := g.ask()
+	fmt.Printf("Your guess is: %s\n",
+		string(guess))
 }
