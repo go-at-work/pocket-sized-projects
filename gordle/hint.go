34e1a9647b71127be39bc86d05a11485e10ecdb3
arisromil <arisromil@yahoo.com>
2025-07-29 12:52:03 -0600

    Create hint.go

---
+++ gordle/hint.go
@@ -0,0 +1,57 @@
+package gordle
+
+import "strings"
+
+type hint int
+
+const (
+	absentCharacter hint = iota
+	wrongPosition
+	correctPosition
+)
+
+func (h hint) String() string {
+	switch h {
+	case absentCharacter:
+		return "⬜️"
+	case wrongPosition:
+		return "🟡"
+	case correctPosition:
+		return "💚"
+	default:
+		// This should never happen.
+		return "💔"
+	}
+}
+
+// feedback is a list of hints, one per character of the word.
+type feedback []hint
+
+// String implements the Stringer interface for a slice of hints.
+func (fb feedback) String() string {
+	sb := strings.Builder{}
+	for _, h := range fb {
+		sb.WriteString(h.String())
+	}
+	return sb.String()
+}
+
+func (fb feedback) StringConcat() string {
+	var output string
+	for _, h := range fb {
+		output += h.String()
+	}
+	return output
+}
+
+func (fb feedback) Equal(other feedback) bool {
+	if len(fb) != len(other) {
+		return false
+	}
+	for index, value := range fb {
+		if value != other[index] {
+			return false
+		}
+	}
+	return true
+}
